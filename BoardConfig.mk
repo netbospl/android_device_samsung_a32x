3d144e13c52b136dc2a8bd5cef182ff3834368ca
Piotr Matela <piotrekmat@protonmail.com>
2021-06-12 21:06:21 +0100

    used twrp builder and added few manually selected lines

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,33 +1,51 @@
-# inherit from the proprietary version
--include vendor/samsung/a32x/BoardConfigVendor.mk
-# -include vendor/samsung/a32x/BoardConfigKernel.mk
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 DEVICE_PATH := device/samsung/a32x
 
-USE_CAMERA_STUB := true
 # For building with minimal manifest
-# ALLOW_MISSING_DEPENDENCIES := true
+ALLOW_MISSING_DEPENDENCIES := true
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT := armv8-2a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := cortex-a55
+TARGET_CPU_VARIANT_RUNTIME := cortex-a55
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
-TARGET_BOARD_SUFFIX := _64
+TARGET_2ND_CPU_VARIANT := cortex-a55
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
+
 TARGET_USES_64_BIT_BINDER := true
 
-# Assert
-TARGET_OTA_ASSERT_DEVICE := a32x
+TARGET_CPU_SMP := true
+ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
+
+ALLOW_MISSING_DEPENDENCIES := true
 
 # Bootloader"
-TARGET_BOOTLOADER_BOARD_NAME := SRPTF26A001
+TARGET_BOOTLOADER_BOARD_NAME := SRPTF26A002
 
 # File systems
 BOARD_HAS_LARGE_FILESYSTEM := true
@@ -37,7 +55,10 @@ BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
-TARGET_COPY_OUT_VENDOR := vendor
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6853
+TARGET_BOARD_PLATFORM_GPU := mali-g57
 
 # Kernel
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
@@ -60,25 +81,39 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/samsung/a32x
 TARGET_KERNEL_CONFIG := a32x_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-TARGET_KERNEL_CLANG_COMPILE := true
 
 # Platform
 TARGET_BOARD_PLATFORM := mt6853
 
 # Recovery
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
 
 # Hack: prevent anti rollback
-# PLATFORM_SECURITY_PATCH := 2099-12-31
-# VENDOR_SECURITY_PATCH := 2099-12-31
-# PLATFORM_VERSION := 16.1.0
+PLATFORM_SECURITY_PATCH := 2099-12-31
+VENDOR_SECURITY_PATCH := 2099-12-31
+PLATFORM_VERSION := 16.1.0
 
-# TWRP Configuration
-# TW_THEME := portrait_hdpi
-# TW_EXTRA_LANGUAGES := true
-# TW_SCREEN_BLANK_ON_BOOT := true
-# TW_INPUT_BLACKLIST := "hbtp_vm"
-# TW_USE_TOOLBOX := true
-TARGET_MTK_KERNEL := true
-MTK_BOARD_PLATFORMS += mt6853
+# TWRP specific build flags
+TW_DEVICE_VERSION := 1_netbospl
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
+TW_MAX_BRIGHTNESS := 306
+TW_DEFAULT_BRIGHTNESS := 200
+TW_Y_OFFSET := 50
+TW_H_OFFSET := -50
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_INCLUDE_NTFS_3G := true
+TW_USE_NEW_MINADBD := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_USE_TOOLBOX := true
+TARGET_USES_MKE2FS := true
+TW_NO_LEGACY_PROPS := true
+TW_NO_BIND_SYSTEM := true
+TW_NO_SCREEN_BLANK := true
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
--- device.mk
+++ device.mk
@@ -1,28 +1,27 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
+LOCAL_PATH := device/samsung/a32x
 
-$(call inherit-product-if-exists, vendor/samsung/a32x/a32x-vendor.mk)
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-DEVICE_PACKAGE_OVERLAYS += device/samsung/a32x/overlay
-
-
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/samsung/a32x/prebuilt/Image.gz
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
-
-$(call inherit-product, build/target/product/full.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_a32x
-PRODUCT_DEVICE := a32x
-PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-A326B
-PRODUCT_MANUFACTURER := samsung
-PRODUCT_RELEASE_NAME := samsung a32x
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
